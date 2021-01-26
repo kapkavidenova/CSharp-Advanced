5d9471474d6a35ae786279dbe3e3677c989bccb5
kapka videnova <videnovakapka@gmail.com>
2021-01-26 17:03:53 +0200

    Added labs

---
+++ MultiDimensionalArrays/SquareWithMaximumSum/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            int[] dimensions = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < rows-n+1; row++)
+            {
+                for (int col = 0; col < cols-n+1; col++)
+                {
+                   int sum = matrix[row, col] + matrix[row, col+1]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+            for (int i = maxRow; i < maxRow + n; i++)
+            {
+                for (int j = maxCol; j < maxCol + n; j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
