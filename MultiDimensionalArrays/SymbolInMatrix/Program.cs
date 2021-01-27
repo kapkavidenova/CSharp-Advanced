09154ff64729214008a700601c4018fb6cb041cf
kapka videnova <videnovakapka@gmail.com>
2021-01-27 10:09:52 +0200

    Added homework

---
+++ MultiDimensionalArrays/SymbolInMatrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] currentRow = Console.ReadLine().ToArray();
+  
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currentRow[col];                   
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            bool isFound = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {                  
+                    if (matrix[i, j] == symbol)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        isFound = true;
+                        return;
+                    }
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
+
