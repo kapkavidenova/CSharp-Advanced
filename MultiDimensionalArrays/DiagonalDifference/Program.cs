dcadc721f1e8d7ec927ced34411d476674cbf7ba
kapka videnova <videnovakapka@gmail.com>
2021-01-29 15:44:29 +0200

    Added homework

---
+++ MultiDimensionalArrays/DiagonalDifference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+
+                    if (row == col)
+                    {
+                        sum1 += matrix[row, col];
+                    }
+                    if (col == n - 1 - row)
+                    {
+                        sum2 += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sum1 - sum2));
+        }
+    }
+}
