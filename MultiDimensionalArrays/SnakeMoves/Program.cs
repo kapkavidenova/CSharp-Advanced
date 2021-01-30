eb2ad672beaa29b6452be370ab23c47d27307e85
kapka videnova <videnovakapka@gmail.com>
2021-01-30 11:13:58 +0200

    Added exercises

---
+++ MultiDimensionalArrays/SnakeMoves/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            string snake = Console.ReadLine();
+            char[,] snakes = new char[rows, cols];
+            int currentLetter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        snakes[row, col] = snake[currentLetter];
+                        currentLetter++;
+
+                        if (currentLetter == snake.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        snakes[row, col] = snake[currentLetter];
+                        currentLetter++;
+
+                        if (currentLetter == snake.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(snakes[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
