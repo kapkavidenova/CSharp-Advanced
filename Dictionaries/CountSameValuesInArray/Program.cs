1bd4dde00c6f6deeeafd44f7a336fbd287f82bff
kapka videnova <videnovakapka@gmail.com>
2021-02-06 00:29:31 +0200

    Added exercises

---
+++ Dictionaries/CountSameValuesInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+
+            for (int i = 0; i <nums.Length; i++)
+            {
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], 0);
+                }
+                dict[nums[i]]++;
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
