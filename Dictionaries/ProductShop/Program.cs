2047e54fb8914f28af079278d01581862cf3b46a
kapka videnova <videnovakapka@gmail.com>
2021-02-08 16:08:29 +0200

    Added exercises

---
+++ Dictionaries/ProductShop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Globalization;
+
+namespace ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            while (input != "Revision")
+            {
+                string[] info = input.Split(", ");
+                string shop = info[0];
+                string product = info[1];
+                double price = double.Parse(info[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+                if (!shops[shop].ContainsKey(product))
+                {
+                    shops[shop].Add(product, price);
+                }
+                
+                input = Console.ReadLine();
+            }
+            shops = shops.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in shops)
+            {
+                Console.WriteLine($"{item.Key}->");
+
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
