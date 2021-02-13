167a053d43a99309a0195b2fbf4200faabbf3f5d
kapka videnova <videnovakapka@gmail.com>
2021-02-13 17:09:36 +0200

    Added homework

---
+++ Dictionaries/SetsOfElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < numbers[0]; i++)
+            {
+                int num = int.Parse(Console.ReadLine());               
+                firstSet.Add(num);
+            }
+
+            for (int i = 0; i < numbers[1]; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondSet.Add(num);
+            }
+              
+            var result = firstSet.Intersect(secondSet);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
