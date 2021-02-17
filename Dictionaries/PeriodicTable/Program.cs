dc3eec8907d95cc38495f573936bdfe96dfd9ea3
kapka videnova <videnovakapka@gmail.com>
2021-02-17 09:59:01 +0200

    Added exercises

---
+++ Dictionaries/PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> set = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] compounds = Console.ReadLine().Split().ToArray();
+                for (int j = 0; j < compounds.Length; j++)
+                {
+                    set.Add(compounds[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", set));
+        }
+    }
+}
