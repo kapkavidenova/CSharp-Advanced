08ea44111065635d419280081485e590f6c896ac
kapka videnova <videnovakapka@gmail.com>
2021-02-17 09:58:24 +0200

    Added homework

---
+++ Dictionaries/EvenTimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!nums.ContainsKey(number))
+                {
+                    nums.Add(number, 0);
+                }
+                nums[number]++;
+            }
+            var result = nums.Where(x => x.Value % 2 == 0).FirstOrDefault().Key;
+            Console.WriteLine(result);
+        }
+    }
+}
