89a3d55966d153651314b2d736c90242b0ccea11
kapka videnova <videnovakapka@gmail.com>
2021-02-21 15:39:41 +0200

    Added exercises

---
+++ DefiningClasses/ClassConstructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Car bmv = new Car("BMV", "X6", 1993, 5003, -50);
+            Car defaultGolf = new Car();
+
+            Console.WriteLine($"Default golf: " + defaultGolf.WhoAmI());
+
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+            //Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
