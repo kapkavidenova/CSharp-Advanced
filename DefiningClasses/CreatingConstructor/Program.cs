0adbddde3cabe73db6aba3b725da9752bceb5980
kapka videnova <videnovakapka@gmail.com>
2021-02-24 17:49:32 +0200

    Added exercises

---
+++ DefiningClasses/CreatingConstructor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person("Ana", 20);
+            Console.WriteLine(personOne.Name + " " + "-" + personOne.Age);
+        }
+    }
+}
