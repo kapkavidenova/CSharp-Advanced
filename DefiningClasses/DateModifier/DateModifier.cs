a8cb03b3b9e16c0196eb3b00ad25b8a531ddba62
kapka videnova <videnovakapka@gmail.com>
2021-02-24 17:50:18 +0200

    Added exercises

---
+++ DefiningClasses/DateModifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+        public static int GetDifference(string firstDate, string secondDate)
+        {
+            //DateTime dateOne = new DateTime();
+
+            DateTime firsttDate = DateTime.Parse(firstDate);
+            DateTime secondtDate = DateTime.Parse(secondDate);
+
+            TimeSpan diff = firsttDate - secondtDate;
+            return Math.Abs(diff.Days);
+
+        }
+    }
+}
---
+++ DefiningClasses/DateModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firsttDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.GetDifference(firsttDate, secondDate);
+            Console.WriteLine(days);
+        }
+    }
+}
