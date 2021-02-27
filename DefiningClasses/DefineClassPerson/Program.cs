b86f4daf046f14a07b23b8f6c5ec36a62a6fd60a
kapka videnova <videnovakapka@gmail.com>
2021-02-27 14:40:50 +0200

    Added exercises

---
+++ DefiningClasses/DefineClassPerson/Program.cs
@@ -0,0 +1,27 @@
+﻿//using DefiningClasses;
+using System;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Pesho";
+            person.Age = 20;
+
+            Person secondPerson = new Person();
+            secondPerson.Name = "Gosho";
+            secondPerson.Age = 18;
+
+            Person thirdPerson = new Person();
+            thirdPerson.Name = "Stamat";
+            thirdPerson.Age = 43;
+
+            Console.WriteLine($"{ person.Name} - { person.Age}");
+            Console.WriteLine($"{ secondPerson.Name} - {secondPerson.Age}");
+            Console.WriteLine($"{  thirdPerson.Name} - {  thirdPerson.Age}");
+        }
+    }
+}
