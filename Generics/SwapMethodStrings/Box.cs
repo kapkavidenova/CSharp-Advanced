dd7245cd6c79ba61fa7f80e55c7ee4d4186d1760
kapka videnova <videnovakapka@gmail.com>
2021-03-03 09:41:51 +0200

    Added exercises

---
+++ Generics/SwapMethodStrings/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerixBox
+{
+    public class Box(T)
+  {
+        public Box(List<T> value)
+    {
+        this.Values = value;
+    }
+    public List<T> Values { get; set; }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var item in this.Values)
+        {
+            sb.AppendLine($"{item.GetType()}: {item}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public void Swap(List<T> items, int firstIndex, int secondIndex)
+    {
+        T temp = items[firstIndex];
+        items[firstIndex] = items[secondIndex];
+        items[secondIndex] = temp;
+    }
+
+
+}
+}
