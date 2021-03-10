f4d0f7a4729e095c84c216ed87ee918844a485dc
kapka videnova <videnovakapka@gmail.com>
2021-03-10 12:19:54 +0200

    Added exercises

---
+++ IteratorsAndComparators/Froggy/Lake.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+
+        public Lake(int[] stones)
+        {
+            this.Stones = stones;
+        }
+
+
+        public int[] Stones { get; set; }
+
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int lastEvenIndex = 0;
+
+            for (int i = 0; i < this.Stones.Length; i += 2)
+            {
+                lastEvenIndex = i;
+
+                yield return this.Stones[i];
+            }
+
+
+
+
+            int firstOddIndex;
+
+            if (lastEvenIndex == this.Stones.Length - 1)
+            {
+                firstOddIndex = lastEvenIndex - 1;
+            }
+            else
+            {
+                firstOddIndex = lastEvenIndex + 1;
+            }
+
+
+            for (int j = firstOddIndex; j >= 0; j -= 2)
+            {
+                yield return this.Stones[j];
+
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators/Froggy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(
+               Console.ReadLine()
+               .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray());
+
+            var path = new List<int>();
+
+            foreach (var stone in lake)
+            {
+                path.Add(stone);
+            }
+
+            Console.WriteLine(string.Join(", ", path));
+
+
+        }
+    }
+}
