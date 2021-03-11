f75ce7cffa95c5f53f461ea3f1e74bbd2caefce0
kapka videnova <videnovakapka@gmail.com>
2021-03-11 15:45:01 +0200

    Added exercises

---
+++ IteratorsAndComparators/ListyIterator/Program.cs
@@ -0,0 +1,56 @@
+﻿using ListyIterator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ListyIterator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            var listyIterator = new ListyIterator<string>(command);
+
+            while (true)
+            {
+                string token = Console.ReadLine();
+
+                if (token == "END")
+                {
+                    break;
+                }
+
+                try
+                {
+                    if (token == "Move")
+                    {
+                        Console.WriteLine(listyIterator.Move());
+                    }
+                    else if (token == "HasNext")
+                    {
+                        Console.WriteLine(listyIterator.HasNext());
+                    }
+                    else if (token == "Print")
+                    {
+                        listyIterator.Print();
+                    }
+                    else if (token == "PrintAll")
+                    {
+                        listyIterator.PrintAll();
+                    }
+
+                }
+                catch (InvalidOperationException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
