1835b0c4c78a84bfaeff24c3eed8f4c50fe2f1ea
kapka videnova <videnovakapka@gmail.com>
2021-03-28 19:47:31 +0300

    Added exercises

---
+++ OOP2/Imheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SportCar car = new SportCar(200, 100);
+
+            car.Drive(25);
+            
+        }
+    }
+}
