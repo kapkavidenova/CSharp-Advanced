5ae1347cb108d241b2d87eef008717df7da42d6d
kapka videnova <videnovakapka@gmail.com>
2021-03-29 16:05:17 +0300

    Added exercises

---
+++ OOP2/Imheritance/Restaurant/Coffee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultMilliters = 50;
+
+        private const decimal DefaultPrice = 3.50m;
+
+        
+        public Coffee(string name, double caffeine) 
+            : base(name, DefaultPrice, DefaultMilliters)
+        {
+            this.Caffeint = caffeine;
+        }
+        public double CoffeeMilliters => DefaultMilliters;
+
+        public decimal CoffeePrice => DefaultPrice;
+
+        public double Caffeint { get; set; }
+    }
+}
