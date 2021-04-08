b5f6db16c6a4a364f04fb431f8dd6f04b4fc99da
kapka videnova <videnovakapka@gmail.com>
2021-04-08 16:56:59 +0300

    Added exercises

---
+++ New folder/InterfacesAndAbstraction/BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable
+    {
+        //public Citizen(string name, int age, string birthdate)
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    Birthdate = birthdate;
+        //}
+
+        public Citizen(string id, string name, int age, string birthdate)
+        {
+            Id = Id;
+            Name = name;
+            Age = age;
+            Birthdate = birthdate;
+        }
+
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
