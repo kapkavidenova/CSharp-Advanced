b39305f83917702749daadfc4798c3dbf1f6bf15
kapka videnova <videnovakapka@gmail.com>
2021-04-15 09:41:37 +0300

    Added exercises

---
+++ OOP/InterfacesAndAbstraction/ExplicitInterfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input!="Ënd")
+            {
+                string[] info = input.Split();
+
+                string name = info[0];
+                string country = info[1];
+                int age = int.Parse(info[2]);
+
+            Citizen citizen = new Citizen(name, country, age);
+
+                IResident resident = citizen;
+                IPerson person = citizen;
+
+            Console.WriteLine(person.GetName());
+            Console.WriteLine(resident.GetName());
+
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
