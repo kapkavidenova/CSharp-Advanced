e2e57a3611b34de0ab177031d10b51f01fda89d9
kapka videnova <videnovakapka@gmail.com>
2021-04-27 09:46:20 +0300

    Added exercises

---
+++ OOP/InterfacesAndAbstraction/MultipleImplementation/Citizen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+
+        public Citizen(string name, int age, string id, string birtdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birtdate;
+
+        }
+        public string Name { get; private set; }
+
+        public int Age { get ; private set ; }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+       
+    }
+}
